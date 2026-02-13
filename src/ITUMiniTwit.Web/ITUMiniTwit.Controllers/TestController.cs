f36d95390891d1ae91c79634e6daac646b7778f7
alhhITU <alhh@itu.dk>
2026-02-14 00:04:38 +0100

    Finished the HttpPost for /fllws/{username}.
    Error handling added, and using [FromBody] and FollowAction (which contains a string for Follow, and Unfollow), it should be able to execute follow or unfollow logic.
    
    Co-authored-by:
    https://medium.com/@ahmed_eprahim/explanation-of-http-status-codes-in-c-30ffade4e83c
    https://medium.com/@nwonahr/working-with-json-data-in-asp-net-core-web-api-fbc4f0ee39c4

--- src/ITUMiniTwit.Web/ITUMiniTwit.Controllers/TestController.cs
+++ src/ITUMiniTwit.Web/ITUMiniTwit.Controllers/TestController.cs
@@ -11,40 +11,76 @@ using NuGet.Protocol;
 public class TestController : ControllerBase
 {
     private readonly AuthorService _AuthServ;
-
-
     public TestController(AuthorService AuthServ)
     {
         _AuthServ = AuthServ;
     }
 
+
     [HttpGet("{username}")]
     public async Task<IActionResult> Get(string username)
     {
         try { _AuthServ.GetAuthorByName(username); } catch { return NotFound(); }
-        
-        string value = Request.Headers["c2ltdWxhdG9yOnN1cGVyX3NhZmUh"];
-        if(value == null)
+        if (!Authorization()){ 
+            return StatusCode(403, new {status = 0, error_msg = "Not authorized"});
+            }
+
+        List<string> follows = await getFollowingUsernames(username);
+
+        return Ok(new { follows });
+
+    }
+
+    [HttpPost("{username}")]
+    public async Task<IActionResult> Post(string username, [FromBody] FollowAction body)
+    {
+        try { _AuthServ.GetAuthorByName(username); } catch { return NotFound(); }
+        if (!Authorization())
         {
-            var returncode = new {status = 0, error_msg = "Not authorized"};
-            return Unauthorized(returncode);
+            return StatusCode(403, new {status = 0, error_msg = "Not authorized"});
         }
 
-        List<Author> list = await _AuthServ.GetFollowing(username);
+        var user = User.Identity!.Name!;
+        
+        if(body.Follow != null)
+        {
+            await _AuthServ.Follow(user, username);
+        }
+        if(body.Unfollow != null)
+        {
+            await _AuthServ.Unfollow(user,username);
+        }
+
+        return NoContent();
+    }
+
+    public bool Authorization()
+    {
+        string? value = Request.Headers["c2ltdWxhdG9yOnN1cGVyX3NhZmUh"];
+        if(value == null)
+        {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public async Task<List<string>> getFollowingUsernames(string user)
+    {
+        List<Author> list = await _AuthServ.GetFollowing(user);
         List<string> follows = new List<string>();
         foreach (Author x in list)
         {
             follows.Add(x.UserName ?? "");
         }
 
-
-        return Ok(new { follows });
-
+        return follows;
+        
     }
 
-    [HttpPost]
-    public IActionResult Post(string username)
+    public class FollowAction()
     {
-        return Ok(new { message = "hello" });
+        public string? Follow {get; set;}
+        public string? Unfollow {get; set;}
     }
 }
