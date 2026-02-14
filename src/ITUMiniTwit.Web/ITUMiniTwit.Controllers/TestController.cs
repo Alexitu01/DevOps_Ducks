3b3c97d01b9594be519f12cdb7c58b03237f906b
alhhITU <alhh@itu.dk>
2026-02-14 23:10:06 +0100

    Made the HttpGet for /msgs.
    Also fixed the authorization method to actively account for the Basic Authentication crednetials.
    
    Co-Authored-by:
    https://stackoverflow.com/questions/25855698/how-can-i-retrieve-basic-authentication-credentials-from-the-header

--- src/ITUMiniTwit.Web/ITUMiniTwit.Controllers/TestController.cs
+++ src/ITUMiniTwit.Web/ITUMiniTwit.Controllers/TestController.cs
@@ -1,16 +1,19 @@
 using System.Net;
+using System.Text;
 using System.Text.Json.Nodes;
+using ITUMiniTwit.Core;
 using ITUMiniTwit.Core.Models;
 using ITUMiniTwit.Infrastructure.ITUMiniTwit.Repositories;
 using ITUMiniTwit.Infrastructure.ITUMiniTwit.Service;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
 using Newtonsoft.Json.Linq;
 using NuGet.Protocol;
 
 
 public static class LatestState
 {
-    public static int state = 0;
+    public static int? state = null;
 }
 
 [ApiController]
@@ -28,14 +31,15 @@ public class fllwsController : ControllerBase
     public async Task<IActionResult> Get(string username, [FromQuery] int? latest)
     {
         try { _AuthServ.GetAuthorByName(username); } catch { return NotFound(); }
-        if (!Authorization()){ 
-            return StatusCode(403, new {status = 0, error_msg = "Not authorized"});
-            }
+        if (!Authorization())
+        {
+            return StatusCode(403, new { status = 0, error_msg = "Not authorized" });
+        }
 
         List<string> follows = await getFollowingUsernames(username);
-        if(latest != null)
+        if (latest != null)
         {
-            LatestState.state = (int) latest; 
+            LatestState.state = (int)latest;
         }
         return Ok(new { follows });
 
@@ -47,22 +51,22 @@ public class fllwsController : ControllerBase
         try { _AuthServ.GetAuthorByName(username); } catch { return NotFound(); }
         if (!Authorization())
         {
-            return StatusCode(403, new {status = 0, error_msg = "Not authorized"});
+            return StatusCode(403, new { status = 0, error_msg = "Not authorized" });
         }
 
         var user = User.Identity!.Name!;
-        
-        if(body.Follow != null)
+
+        if (body.Follow != null)
         {
             await _AuthServ.Follow(user, username);
         }
-        if(body.Unfollow != null)
+        if (body.Unfollow != null)
         {
-            await _AuthServ.Unfollow(user,username);
+            await _AuthServ.Unfollow(user, username);
         }
-        if(latest != null)
+        if (latest != null)
         {
-            LatestState.state = (int) latest; 
+            LatestState.state = (int)latest;
         }
 
         return NoContent();
@@ -70,13 +74,18 @@ public class fllwsController : ControllerBase
 
     public bool Authorization()
     {
-        string? value = Request.Headers["c2ltdWxhdG9yOnN1cGVyX3NhZmUh"];
-        if(value == null)
+        string? authHeader = Request.Headers["Authorization"];
+        if (authHeader == null || !authHeader.StartsWith("Basic"))
         {
             return false;
-        } else {
-            return true;
         }
+
+        string encodedUsernamePassword = authHeader.Substring("Basic ".Length).Trim();
+        if (encodedUsernamePassword == "c2ltdWxhdG9yOnN1cGVyX3NhZmUh")
+        {
+            return true;
+        }else{
+            return false;}
     }
 
     public async Task<List<string>> getFollowingUsernames(string user)
@@ -87,15 +96,13 @@ public class fllwsController : ControllerBase
         {
             follows.Add(x.UserName ?? "");
         }
-
         return follows;
-        
     }
 
     public class FollowAction()
     {
-        public string? Follow {get; set;}
-        public string? Unfollow {get; set;}
+        public string? Follow { get; set; }
+        public string? Unfollow { get; set; }
     }
 }
 
@@ -103,30 +110,102 @@ public class fllwsController : ControllerBase
 [Route("latest")]
 public class latestController : ControllerBase
 {
-    AuthorService _AuthServ;
-
-    public latestController(AuthorService AuthServ)
-    {
-        _AuthServ = AuthServ;
-    }
-
     [HttpGet("")]
     public IActionResult Get()
     {
-        return Ok(new {latest = LatestState.state});
+        try
+        {
+            return Ok(new { latest = LatestState.state });
+        }
+        catch
+        {
+            return StatusCode(500, new { status = 0, error_msg = "Unexpected Error" });
+        }
     }
-    
 }
 
 [ApiController]
 [Route("msgs")]
 public class messageController : ControllerBase
-{  
+{
+    CheepService _CheepServ;
+    public messageController(CheepService CheepServ)
+    {
+        _CheepServ = CheepServ;
+    }
+
+    [HttpGet("")]
+    public IActionResult Get([FromQuery(Name = "no")] int? num, [FromQuery(Name = "latest")] int? latest)
+    {
+        if (!Authorization())
+        { return StatusCode(403, new { status = 0, error_msg = "Not authorized" }); }
+
+        List<CheepDto> list = new List<CheepDto>();
+        if (num != null)
+        {
+            list = _CheepServ.GetCheeps(1, (int)num);
+        }
+        else
+        {
+            list = _CheepServ.GetCheeps(1, 100);
+        }
+
+        List<messageInfo> messageInfos = new List<messageInfo>();
+        foreach (CheepDto Dto in list)
+        {
+            messageInfos.Add(new messageInfo
+            {
+                content = Dto.Text,
+                pub_date = Dto.TimeStamp,
+                user = Dto.Author
+            });
+        }
+
+        if (latest != null)
+        { LatestState.state = (int)latest; }
+
+        return Ok(messageInfos);
+    }
+
+    [HttpGet("{username}")]
+    public IActionResult GetWithUsername()
+    {
+        return Ok(null);
+    }
+
+    [HttpPost("{username}")]
+    public IActionResult Post()
+    {
+        return Ok(null);
+    }
+
+    public class messageInfo
+    {
+        public string? content { get; set; }
+        public string? pub_date { get; set; }
+        public string? user { get; set; }
+    }
+
+    public bool Authorization()
+    {
+        string? authHeader = Request.Headers["Authorization"];
+        if (authHeader == null || !authHeader.StartsWith("Basic"))
+        {
+            return false;
+        }
+
+        string encodedUsernamePassword = authHeader.Substring("Basic ".Length).Trim();
+        if (encodedUsernamePassword == "c2ltdWxhdG9yOnN1cGVyX3NhZmUh")
+        {
+            return true;
+        }else{
+            return false;}
+    }
 }
 
 [ApiController]
 [Route("register")]
 public class registerController : ControllerBase
 {
-    
+
 }
