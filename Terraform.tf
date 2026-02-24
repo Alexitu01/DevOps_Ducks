eea8fe1736dc5685dc7cc5add3a974c4a19e632f
Nhelge <nanhe@itu.dk>
2026-02-24 20:37:31 +0100

    Added missing comma

--- Terraform.tf
+++ Terraform.tf
@@ -45,7 +45,7 @@ resource "digitalocean_droplet" "web" {
       "cd DevOps_Ducks",
       "cd src/ITUMiniTwit.Web",
       "dotnet dev-certs https --trust",
-      "cd ../.."
+      "cd ../..",
       "sudo docker build -f Dockerfile -t itu-minitwit .",
       "sudo docker run -d -p 80:80 itu-minitwit" 
       #Changed the Docker port from 80:80 to 80:8080. Droplet Console showed it was running on 8080
