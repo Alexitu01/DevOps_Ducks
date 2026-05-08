70046e5dbb05c72650e226b66dc7d69849b05d95
Nanna <nanna.helge@gmail.com>
2026-05-08 16:04:05 +0200

    testing multiple hosts

--- scripts/deploy_all.sh
+++ scripts/deploy_all.sh
@@ -3,8 +3,8 @@
 VM1="root@$VM1_HOST"
 VM2="root@$VM2_HOST"
 
-ssh $VM1 "bash deploy.sh" &&
-ssh $VM2 "bash deploy.sh"
+ssh $VM1 "bash scripts/deploy.sh" &&
+ssh $VM2 "bash scripts/deploy.sh"
 wait
 
 echo "Update done"
