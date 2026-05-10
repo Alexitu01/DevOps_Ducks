e207db89b83f58bf4f8904eef94ae39b68b1fa41
Nanna <nanna.helge@gmail.com>
2026-05-10 06:31:45 +0200

    fix: moved env variables to the last step in release.yml

--- scripts/deploy_all.sh
+++ scripts/deploy_all.sh
@@ -3,8 +3,8 @@
 VM1="root@$VM1_HOST"
 VM2="root@$VM2_HOST"
 
-ssh $VM1 "bash scripts/deploy.sh" &&
-ssh $VM2 "bash scripts/deploy.sh"
+ssh $VM1 "bash scripts/deploy.sh" &
+ssh $VM2 "bash scripts/deploy.sh" &
 wait
 
 echo "Update done"
