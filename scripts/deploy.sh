41ba3cdf504af34a831904a29ed864b4ee6cb4aa
Nanna <nanna.helge@gmail.com>
2026-05-10 21:41:40 +0200

    added spaces

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,7 +10,7 @@ else
     NEW_VERSION="blue"
 fi
 
-if [ "$NEW_VERSION"="blue" ]; then
+if [ "$NEW_VERSION" = "blue" ]; then
     PORTS=(8081 8082)
 else
     PORTS=(8083 8084)
@@ -25,7 +25,7 @@ for PORT in "${PORTS[@]}"; do
     curl -sf http://127.0.0.1:$PORT || { echo "Health check failed on port $PORT"; exit 1; }
 done
 
-echo "proxy_pass http://${NEW_VERSION};" > /etc/nginx/active_upstream.conf
+echo "proxy_pass http://${NEW_VERSION};" | sudo tee /etc/nginx/active_upstream.conf > /dev/null
 sudo nginx -t && sudo systemctl reload nginx
 
 bash ../change_color.sh
