7a2ef972e1aa7b84e112af3be45091744e2af797
Nanna <nanna.helge@gmail.com>
2026-05-11 04:56:20 +0200

    added a line to remove old docker containers and prolonged sleep to 30

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -20,12 +20,14 @@ docker compose pull ${NEW_VERSION}server1 ${NEW_VERSION}server2
 docker compose up -d --build ${NEW_VERSION}server1 ${NEW_VERSION}server2 --remove-orphans
 
 echo "Waiting for healthcheck..."
-sleep 5
+sleep 30
 for PORT in "${PORTS[@]}"; do
     curl -sf http://127.0.0.1:$PORT || { echo "Health check failed on port $PORT"; exit 1; }
 done
 
 echo "proxy_pass http://${NEW_VERSION};" | sudo tee /etc/nginx/active_upstream.conf > /dev/null
 sudo nginx -t && sudo systemctl reload nginx
+
+docker compose rm -sf ${CURRENT}server1 ${CURRENT}server2
 
 echo "$NEW_VERSION" > ../current_color
