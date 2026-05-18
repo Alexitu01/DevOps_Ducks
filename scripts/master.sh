867de326b2f554df792f1a00c6823029f6d33908
Alexander Hvalsøe Holst <alhh@itu.dk>
2026-05-18 21:23:22 +0200

    Update conditional syntax in master.sh

--- scripts/master.sh
+++ scripts/master.sh
@@ -4,7 +4,7 @@ IP='68.183.242.15'
 ID=$(curl -s http://169.254.169.254/metadata/v1/id)
 HAS_RESERVED_IP=$(curl -s http://169.254.169.254/metadata/v1/reserved_ip/ipv4/active)
 
-if [ $HAS_RESERVED_IP = "false" ]; then
+if [[ $HAS_RESERVED_IP = "false" ]]; then
     n=0
     while [ $n -lt 5 ]
     do
